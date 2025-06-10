0795c547130542bda8397213ded502c8a0f72374
changatz <141410334+changatz@users.noreply.github.com>
2025-06-10 14:17:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('This is a placeholder for functionality such as linking to the backend.');
+    });
+});
